e262fd1e0d6a05bd5f25138670210babe5604ad5
rafikulislamrafi <mohammadrafe0099@gmail.com>
2025-07-20 10:56:10 +0600

    Initial commit

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller
+{
+    function index()
+    {
+        return view('frontend.index');
+    }
+
+    function about()
+    {
+        return view('frontend.about');
+    }
+}
